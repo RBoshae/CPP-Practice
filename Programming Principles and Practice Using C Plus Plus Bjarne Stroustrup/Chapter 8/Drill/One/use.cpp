fa4c3e49b66b2a7e943bd6d94ac8253e1a4b5849
Wanderrer <rwboshae@gmail.com>
2018-06-22 15:50:52 -0700

    Added skeleton code to use.cpp

---
+++ Programming Principles and Practice Using C Plus Plus Bjarne Stroustrup/Chapter 8/Drill/One/use.cpp
@@ -0,0 +1,4 @@
+#include "my.h"
+// NOTE: use.cpp does not use #include std_lib_facilities.h as it doesn't directly use any of those facilities.
+
+// TODO: Define main() to set the value of foo to 7 and print it using print_foo(), and print the value of 99 using print().
