590ac8a9003008555c76c3a6e96358f32736e4bc
Wanderrer <rwboshae@gmail.com>
2018-06-22 15:47:56 -0700

    Added TODO skeleton

---
+++ Programming Principles and Practice Using C Plus Plus Bjarne Stroustrup/Chapter 8/Drill/One/my.cpp
@@ -0,0 +1,6 @@
+#include "my.h"
+#include "std_lib_facilities"
+
+// TODO: define print_foo() to print the value of foo using cout.
+
+// TODO: define print(int i) to print the value of i using cout
