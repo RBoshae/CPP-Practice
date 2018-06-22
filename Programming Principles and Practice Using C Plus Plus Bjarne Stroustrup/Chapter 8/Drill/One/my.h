bb90cdbeb93e8a830b99004faf1f0109c14a8763
Wanderrer <rwboshae@gmail.com>
2018-06-22 16:00:13 -0700

    Added comments

--- Programming Principles and Practice Using C Plus Plus Bjarne Stroustrup/Chapter 8/Drill/One/my.h
+++ Programming Principles and Practice Using C Plus Plus Bjarne Stroustrup/Chapter 8/Drill/One/my.h
@@ -1,3 +1,3 @@
-extern int foo;     // declaration
+extern int foo;     // declaration - The use of extern simply states this declaration of foo is not a definition.
 void print_foo();   // declaration
 void print(int);    // declaration
